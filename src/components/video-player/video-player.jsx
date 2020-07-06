83ab18719e98905e258602fda326871917d8701f
Sergey L <sergeylysenko1406@gmail.com>
2020-07-06 14:38:34 +0000

    Создан компонент video-player, к мокам добавлен привью видео

---
+++ src/components/video-player/video-player.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default VideoPlayer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {isPlaying: false}
+  }
+  render() {
+    
+  }
+}
