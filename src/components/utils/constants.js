9e0791239c37cb3ea4663d5c7e847f6db3029c6b
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2020-07-14 00:20:21 +0300

    исправлен экспорт констант

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,19 +1,19 @@
 import PropTypes from "prop-types";
 
-const VideoPreview = {
+export const VideoPreview = {
   IS_MUTED: true,
   WIDTH: 280,
   HEIGHT: 175,
   INTERVAL: 1000
 };
 
-const MoviePages = {
+export const MoviePages = {
   OVERVIEW: `Overview`,
   DETAILS: `Details`,
   REVIEWS: `Reviews`,
 };
 
-const Genres = {
+export const Genres = {
   ALL: `All genres`,
   COMEDIES: `Comedies`,
   CRIME: `Crime`,
@@ -26,13 +26,13 @@ const Genres = {
   THRILLERS: `Thrillers`,
 };
 
-const SIMILAR_MOVIES_COUNT = 4;
-const SHOWING_MOVIES_COUNT_ON_START = 8;
-const SHOWING_MOVIES_COUNT_BY_BUTTON = 8;
+export const SIMILAR_MOVIES_COUNT = 4;
+export const SHOWING_MOVIES_COUNT_ON_START = 8;
+export const SHOWING_MOVIES_COUNT_BY_BUTTON = 8;
 
-const MAX_GENRE_LIST = 10;
+export const MAX_GENRE_LIST = 10;
 
-const movieShape = PropTypes.shape({
+export const movieShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
@@ -46,5 +46,3 @@ const movieShape = PropTypes.shape({
   starring: PropTypes.arrayOf(PropTypes.string).isRequired,
   runTime: PropTypes.number.isRequired,
 });
-
-export {VideoPreview, MoviePages, SIMILAR_MOVIES_COUNT, Genres, MAX_GENRE_LIST, movieShape, SHOWING_MOVIES_COUNT_BY_BUTTON, SHOWING_MOVIES_COUNT_ON_START};
