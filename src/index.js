4d1b22a9ec981dda882f146c93bb778ca24902f9
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2020-07-23 21:11:56 +0300

    создан reduser user, запрос на сервер об авторизации

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
 import {reviews} from "./mocks/reviews.js";
@@ -8,16 +8,25 @@ import reducer from "./reducer/reducer.js";
 import {createAPI} from "./api.js";
 import thunk from "redux-thunk";
 import {Operation as DataOperation} from "./reducer/data/data.js";
+import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/user/user.js";
 
-const api = createAPI();
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
-    applyMiddleware(thunk.withExtraArgument(api))
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    )
 );
 
 store.dispatch(DataOperation.loadMovies());
 store.dispatch(DataOperation.loadMainMovie());
+store.dispatch(UserOperation.checkAuthorization());
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/reducer/user/user.js
@@ -0,0 +1,46 @@
+import {extend} from "../../components/utils/utils.js";
+
+const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const ActionType = {
+  REQUIRE_AUTHORIZATION: `REQUIRE_AUTHORIZATION`
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => ({
+    type: ActionType.REQUIRE_AUTHORIZATION,
+    payload: status,
+  }),
+};
+
+const Operation = {
+  checkAuthorization: () => (dispatch, getState, api) => {
+    return api.get(`/login`)
+    .then(() => {
+      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+    })
+    .catch((err) =>{
+      throw (err);
+    });
+  }
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRE_AUTHORIZATION:
+      return extend(state, {
+        authorizationStatus: action.payload,
+      });
+    default:
+      return state;
+  }
+};
+
+export {reducer, Operation, ActionCreator, ActionType, AuthorizationStatus};
